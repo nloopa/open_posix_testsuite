10cc821a2e47deb2eae540e0f12f6f3c16a2cff3
Ingo Weinhold <ingo_weinhold@gmx.de>
2010-04-08 17:32:09 +0000

    * Fixed more incorrect occurrences of the pid_t == int assumption. * Automatic whitespace cleanup.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@36090 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- conformance/interfaces/mmap/6-3.c
+++ conformance/interfaces/mmap/6-3.c
@@ -52,7 +52,7 @@ int main(void)
 	int status;
 	int sig_num;
 
-	snprintf(tmpfname, sizeof(tmpfname), "/tmp/pts_mmap_6_2_%d", getpid());
+	snprintf(tmpfname, sizeof(tmpfname), "/tmp/pts_mmap_6_2_%ld", (long)getpid());
 	unlink(tmpfname);
 	fd = open(tmpfname, O_CREAT | O_RDWR | O_EXCL, S_IRUSR | S_IWUSR);
 	if (fd == -1) {
